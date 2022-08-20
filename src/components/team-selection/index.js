886b29b47b89678df2f725beabbfca3d279c861e
Angelo Reis <angelo.desenvolvedor@gmail.com>
2022-08-20 02:42:09 -0300

    fix: rules and action on form

--- src/components/team-selection/index.js
+++ src/components/team-selection/index.js
@@ -1,44 +1,105 @@
-import React from "react";
+import React, { useCallback, useEffect, useState } from "react";
 
 import PlayerDetail from "../player-info";
 import playersList from "../../players.json";
-export default function TeamSelection() {
-	const [players] = React.useState([...playersList]);
-	const [selectedPlayers, setSelectedPlayers] = React.useState([]);
-	const [showPlayerDetail, setShowPlayerDetail] = React.useState(false);
-	const [idx, setIdx] = React.useState(null);
-	const [welcome, setWelcome] = React.useState(true);
-	const [noBat, setNoBat] = React.useState(0);
-	const [noBowl, setNoBowl] = React.useState(0);
-	const [noAR, setNoAR] = React.useState(0);
-	const [noWk, setNoWK] = React.useState(0);
 
-	const addPlayer = (index) => {
-		return;
-	};
+const BATSMAN = "Batsman"
+const ALLROUNDER = "AllRounder"
+const BOWLER = "Bowler"
+
+export default function TeamSelection() {
+	const [players] = useState([...playersList]);
+	const [selectedPlayers, setSelectedPlayers] = useState([]);
+	const [showPlayerDetail, setShowPlayerDetail] = useState(false);
+	const [idx, setIdx] = useState(null);
+	const [welcome, setWelcome] = useState(true);
+	
+	const addPlayer = useCallback((index) => {
+		const playerToAdd = players[index]
+
+		let counterBatsman = 0
+		let counterAllrounders = 0
+		let counterBowlers = 0
+		let counter = selectedPlayers.length
+
+		selectedPlayers.forEach((element) => {
+			element.type === BATSMAN && counterBatsman++
+			element.type === ALLROUNDER && counterAllrounders++
+			element.type === BOWLER && counterBowlers++
+		})
+
+		if (counter > 10) {
+			alert('Only 11 players are allowed in a team');
+			return;
+		}
+
+		if (counterBatsman > 5 && playerToAdd.type === BATSMAN) {
+			alert('Batsmen can not be more than 6');
+			return;
+		}
+
+		if (counterAllrounders > 3 && playerToAdd.type === ALLROUNDER) {
+			alert('All Rounders can not be more than 4');
+			return;
+		}
+
+		if (counterBowlers > 5 && playerToAdd.type === BOWLER) {
+			alert('Bowlers can not be more than 6');
+			return;
+		}
+		setSelectedPlayers([...selectedPlayers, playerToAdd])
+		setShowPlayerDetail(false)
+	}, [selectedPlayers, showPlayerDetail, players]);
 
 	const removePlayer = (index) => {
-		return
+		const playerByIndex = selectedPlayers[index]
+		setSelectedPlayers(selectedPlayers.filter(player => player.name !== playerByIndex.name))
 	};
 
-	const showplayerDetailsCard = (i) => {
+	const showplayerDetailsCard = (index) => {
+		setIdx(index)
+		setShowPlayerDetail(true)
 		return;
 	};
 
 	const closeCard = () => {
-		return;
+		setShowPlayerDetail(false)
 	};
+
+	const closeWelcome = () => {
+		setWelcome(false)
+	}
+
+	const disabledButton = (index) => {
+		const playerIndexListed = players[index]
+		const playerSelected = selectedPlayers.find(player => player.name === playerIndexListed.name)
+		if (playerSelected) {
+			return true
+		}
+		return false
+	}
+
+	const ModalDetails = () => {
+		if (showPlayerDetail) {
+			return (
+				<PlayerDetail
+					players={players}
+					close={closeCard}
+					index={idx}
+					addPlayer={() => addPlayer(idx)}
+					disabledButton={() => disabledButton(idx)}
+				/>
+			)
+		}
+
+		return <></>
+	}
 
 	return (
 		<div className="mt-50 layout-column justify-content-center align-items-center">
 			<div style={{ display: "flex", width: "80%" }}>
-					<PlayerDetail
-						selectedPlayers={selectedPlayers}
-						i={idx}
-						close={() => closeCard()}
-						index={1}
-						addPlayer={(i) => addPlayer(i)}
-					/>
+
+				<ModalDetails />
 				<div
 					className="card outlined mt-0"
 					style={{
@@ -63,9 +124,10 @@ export default function TeamSelection() {
 								</tr>
 							</thead>
 							<tbody data-testid="available-players-table-body">
+								{welcome && (
 									<tr>
 										<td data-testid="selection-rules" colSpan="3" className="card pb-20">
-											<p data-testid="close-welcome" style={{textAlign:'right'}} onClick={()=>setWelcome(false)}>X</p>
+											<p data-testid="close-welcome" style={{ textAlign: 'right' }} onClick={closeWelcome}>X</p>
 											<h3 style={{ textAlign: "center" }}>
 												<strong>Welcome to Team Selection</strong>
 											</h3>
@@ -77,40 +139,42 @@ export default function TeamSelection() {
 											1-4 All Rounders are allowed in a team
 										</td>
 									</tr>
-										{players.map((player, index) => {
-                                    return (
-                                        <tr
-                                            data-testid={`available-${player.name
-                                                .split(" ")
-                                                .join("-")}-row`}
-                                            key={index}
-                                        >
-                                            <td
-                                                data-testid={`available-${player.name
-                                                    .split(" ")
-                                                    .join("-")}-name`}
-                                                onClick={() => showplayerDetailsCard(index)}
-                                            >
-                                                {player.name}
-                                            </td>
-                                            <td onClick={() => showplayerDetailsCard(index)}>
-                                                {player.type}
-                                            </td>
-                                            <td>
-                                                <button
-                                                    data-testid={`available-${player.name
-                                                        .split(" ")
-                                                        .join("-")}-select`}
-                                                    onClick={() => addPlayer(index)}
-                                                    disabled={selectedPlayers.indexOf(player) !== -1}
-                                                    className="btn btn-primary text"
-                                                >
-                                                    Select
-                                                </button>
-                                            </td>
-                                        </tr>
-                                    );
-                                })}
+								)}
+
+								{players.map((player, index) => {
+									return (
+										<tr
+											data-testid={`available-${player.name
+												.split(" ")
+												.join("-")}-row`}
+											key={index}
+										>
+											<td
+												data-testid={`available-${player.name
+													.split(" ")
+													.join("-")}-name`}
+												onClick={() => showplayerDetailsCard(index)}
+											>
+												{player.name}
+											</td>
+											<td onClick={() => showplayerDetailsCard(index)}>
+												{player.type}
+											</td>
+											<td>
+												<button
+													data-testid={`available-${player.name
+														.split(" ")
+														.join("-")}-select`}
+													onClick={() => addPlayer(index)}
+													disabled={disabledButton(index)}
+													className="btn btn-primary text"
+												>
+													Select
+												</button>
+											</td>
+										</tr>
+									);
+								})}
 							</tbody>
 						</table>
 					</div>
@@ -135,7 +199,7 @@ export default function TeamSelection() {
 								</tr>
 							</thead>
 							<tbody data-testid="selected-players-table-body">
-								{selectedPlayers.map((player, index) => {
+								{selectedPlayers && selectedPlayers.map((player, index) => {
 									return (
 										<tr
 											data-testid={`selected-${player.name
